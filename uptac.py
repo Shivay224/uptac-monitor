3d74b0754f890f5a0854b33111c477e2f13b68be
Shivay224 <shivayp011@gmail.com>
2026-07-05 08:19:55 +0530

    Update uptac.py

--- uptac.py
+++ uptac.py
@@ -3,13 +3,13 @@ import requests
 TELEGRAM_TOKEN = "8942906921:AAGkNJCxt2SfzBBp-ppaYAPHLAorojNypFo"
 CHAT_ID = "8699041887"
 
-def send(msg):
-    r = requests.post(
-        f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage",
-        data={"chat_id": CHAT_ID, "text": msg},
-        timeout=10
-    )
-    print(r.status_code)
-    print(r.text)
+print("STARTING SCRIPT")
 
-send("🔥 TEST 1: GitHub → Telegram working?")
+r = requests.post(
+    f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage",
+    data={"chat_id": CHAT_ID, "text": "🚨 GitHub Actions FINAL TEST"},
+    timeout=10
+)
+
+print("STATUS:", r.status_code)
+print("RESPONSE:", r.text)
