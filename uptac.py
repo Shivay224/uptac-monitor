57f6afafbd375ecffc5110085ba1296c4feaeaff
Shivay224 <shivayp011@gmail.com>
2026-07-05 08:36:30 +0530

    Update uptac.py

--- uptac.py
+++ uptac.py
@@ -1,6 +1,17 @@
-print("SCRIPT STARTED")
-
 import requests
 
-r = requests.get("https://api.telegram.org")
-print("Telegram reachable:", r.status_code)
+TELEGRAM_TOKEN = "8942906921:AAGkNJCxt2SfzBBp-ppaYAPHLAorojNypFo"
+CHAT_ID = "8699041887"
+
+print("SCRIPT STARTED")
+
+url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+
+r = requests.post(
+    url,
+    data={"chat_id": CHAT_ID, "text": "🔥 GITHUB FINAL TELEGRAM TEST"},
+    timeout=10
+)
+
+print("STATUS:", r.status_code)
+print("RESPONSE:", r.text)
